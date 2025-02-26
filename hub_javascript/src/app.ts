685ddec782287da85400c92fa71c168e9ec33bac
guyluz11 <guyluz11@gmail.com>
2025-02-26 18:16:43 +0200

    wip matter

--- hub_javascript/src/app.ts
+++ hub_javascript/src/app.ts
@@ -1,14 +1,17 @@
 
-import {pairDevice, turnOnLight, turnOffLight} from './integrations/matter_integration';
+// import {pairDevice, turnOnLight, turnOffLight} from './integrations/matter_integration';
+import { test } from './integrations/test';
 
 
 
-// The device's pairing code (replace this with the actual code you have)
-const pairingCode = 'MT:Y.K9042C00KA0648G00'; // Replace with your device's pairing code
-const deviceId = '8'; // Replace with your device's actual ID
+// // The device's pairing code (replace this with the actual code you have)
+// const pairingCode = 'MT:Y.K9042C00KA0648G00'; // Replace with your device's pairing code
+// const deviceId = '8'; // Replace with your device's actual ID
 
-pairDevice(pairingCode).then(() => {
-    turnOnLight(deviceId);
-    // Or toggle light state
-    // turnOffLight(deviceId);
-});
+// pairDevice(pairingCode).then(() => {
+//     turnOnLight(deviceId);
+//     // Or toggle light state
+//     // turnOffLight(deviceId);
+// });
+
+test()
